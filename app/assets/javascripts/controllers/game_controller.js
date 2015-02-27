cba548709bcd9d1cc1941710748f4b0a3ceff7d0
Connor Kojimoto <cokojimoto@gmail.com>
2015-02-27 15:06:12 -0800

    adds checks to see if word entered is the same as current word

--- app/assets/javascripts/controllers/game_controller.js
+++ app/assets/javascripts/controllers/game_controller.js
@@ -1,4 +1,3 @@
 app.controller("GameCtrl", ["$scope", function($scope) {
 
-  $scope.data = 'A short story is a brief work of literature, usually written in narrative prose. Emerging from earlier oral storytelling traditions in the 17th century, the short story has grown to encompass a body of work so diverse as to defy easy characterization. At its most prototypical the short story features a small cast of named characters, and focuses on a self-contained incident with the intent of evoking a "single effect" or mood';
 }]);

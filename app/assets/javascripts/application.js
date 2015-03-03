278e8c457d8cc592750f3bad2d2f6502a7a8d30f
Connor Kojimoto <cokojimoto@gmail.com>
2015-03-03 13:43:52 -0800

    displays image, that fades in as more words are correctly typed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require libgif
+//= require rubbable
 //= require turbolinks
 //= require angular
 //= require angular-resource

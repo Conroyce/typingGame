fb9a26534ccb57fef0b99ee700e080854fe9d3de
Connor Kojimoto <cokojimoto@gmail.com>
2015-03-16 19:52:30 -0700

    adds separate navbar for game view

--- app/assets/javascripts/controllers/nav_controller.js
+++ app/assets/javascripts/controllers/nav_controller.js
@@ -1,17 +1,25 @@
-app.controller("NavCtrl", ["$scope","$window", function($scope,$window) {
-  angular.element($window).bind("scroll", function() {
-    if (!$scope.scrollPosition) {
-      $scope.scrollPosition = 0;
-      console.log("created scrollposition")
-    }
+app.controller("NavCtrl", ["$scope","$window","$location", function($scope,$window,$location) {
+  
+  var path = $location.path();
+  if ($location.path() === "/landing") {
+    $scope.mainNav = false;
+    angular.element($window).bind("scroll", function() {
+      if (!$scope.scrollPosition) {
+        $scope.scrollPosition = 0;
+        console.log("created scrollposition")
+      }
 
-    if ($scope.scrollPosition > 50) {
-      $scope.changeNav = true;
-    } else {
-      $scope.changeNav = false;
-    }
-    $scope.scrollPosition = this.pageYOffset;
-    console.log("scrollPosition: ", $scope.scrollPosition);
-    $scope.$apply();
-  })
+      if ($scope.scrollPosition > 50) {
+        $scope.landingNav = true;
+      } else {
+        $scope.landingNav = false;
+      }
+      $scope.scrollPosition = this.pageYOffset;
+      $scope.$apply();
+    })
+  } else {
+    $scope.landingNav = false;
+    $scope.mainNav = true;
+  }
+  
 }]);
